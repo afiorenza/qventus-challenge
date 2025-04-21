303674c874f0ea3615b89ee655d1e2dc65e248a9
afiorenza <agustinfiorenza@gmail.com>
2025-04-21 19:57:57 -0300

    Move to lib configuration

---
+++ src/components/Login.tsx
@@ -0,0 +1,13 @@
+type LoginProps = {
+  options?: any[]
+}
+
+const Login = ({}: LoginProps) => {
+  return (
+    <div className="p-0">
+      Login
+    </div>
+  );
+}
+
+export default Login;
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,31 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 import react from '@vitejs/plugin-react-swc';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  plugins: [react(),  tailwindcss()],
+  plugins: [
+    react(),  
+    tailwindcss()
+  ],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'MyReactComponentLibrary',
+      fileName: (format) => `index.${format === 'es' ? 'es' : 'cjs'}.js`,
+      formats: ['es', 'cjs'],
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom', 'tailwindcss'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
+    },
+    sourcemap: true,
+  },
   server:{ 
     port: 8080
   }
