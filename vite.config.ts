59d3cd393a40251d3c44b28c81d2526bd9bd39e4
afiorenza <agustinfiorenza@gmail.com>
2025-04-22 21:06:12 -0300

    Build types

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,10 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 import react from '@vitejs/plugin-react-swc';
 import tailwindcss from '@tailwindcss/vite';
+import dts from 'vite-plugin-dts';
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), dts()],
   build: {
     lib: {
       entry: resolve(__dirname, 'src/index.tsx'),
